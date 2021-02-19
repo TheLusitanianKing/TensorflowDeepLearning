d6e85eb4717a51a7cdec5a8385a02c2f13385277
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-02-19 22:58:37 +0000

    CNN with the fashion MNIST dataset

---
+++ ch05-convolutional-neural-networks/cnn-fashion-mnist.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+
+# load in the data
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+# scaling
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print(x_train.shape) # N x 28 x 28, N being the number of samples (60000 here)
+
+# the data is 2D, convolution expects height x width x color
+# we need N x 28 x 28 x 1 and not N x 28 x 28
+x_train = np.expand_dims(x_train, -1)
+x_test  = np.expand_dims(x_test, -1)
+
+# number of classes K
+K = len(set(y_test))
+print('number of classes: ', K)
+
+# build the model
+i = Input(shape=x_train[0].shape) # 28 x 28 x 1
+x = Conv2D(32, (3, 3), strides=2, activation='relu')(i)
+x = Conv2D(64, (3, 3), strides=2, activation='relu')(x)
+x = Conv2D(128, (3, 3), strides=2, activation='relu')(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
+
+# compile then fit
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
+
+# plot loss per iteration
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+# plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='accuracy')
+plt.plot(r.history['val_accuracy'], label='val_accuracy')
+plt.legend()
+plt.show()
